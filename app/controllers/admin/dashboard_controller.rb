cdc6d3bf41e08ce54b39de17045eb5e917e9c6c4
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-11-16 18:04:22 -0500

    add feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
-
+  http_basic_authenticate_with name: "Jungle", password: "book"  
+  
   def show
+    @products = Product.all.count
+    @category = Category.all.count
+    
   end
 end
